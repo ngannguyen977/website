725426694e9ddf1d281e973b883f76bbc7401a0e
unknown <ngannguyen977@gmail.com>
2018-01-15 14:00:44 +0700

    update video

--- js/mainScript.js
+++ js/mainScript.js
@@ -10,3 +10,15 @@ $('.count').each(function () {
     });
 });
 
+$(document).ready(function(){
+  $("#myModal").on("hidden.bs.modal",function(){
+    $("#iframeYoutube").attr("src","#");
+  })
+})
+
+function changeVideo(vId){
+  var iframe=document.getElementById("iframeYoutube");
+  iframe.src="https://www.youtube.com/embed/"+vId;
+
+  $("#myModal").modal("show");
+}
